ed5571c98834b02c5921dcba59ca50eefd636176
pf_miles <miles.wy.1@gmail.com>
2013-07-07 22:19:12 +0800

    merge sort mod

--- src/algorithms_in_clojure/merge_sort.clj
+++ src/algorithms_in_clojure/merge_sort.clj
@@ -1,2 +1,23 @@
 (ns algorithms-in-clojure.merge-sort)
 
+(declare ms-merge)
+
+(defn merge-sort [col]
+  (let [size (count col)]
+    (cond
+      (<= size 1) col
+      :else (let [middle (int (/ size 2))
+                  [left right] (split-at middle col)
+                  l-sorted (merge-sort left)
+                  r-sorted (merge-sort right)]
+              (ms-merge l-sorted r-sorted)))))
+
+(defn- ms-merge [left right]
+  (cond
+    (empty? left) right
+    (empty? right) left
+    :else
+      (let [lf (first left) ls (rest left) rf (first right) rs (rest right)]
+        (cond
+          (< lf rf) (cons lf (ms-merge ls right)) 
+          :else (cons rf (ms-merge left rs))))))
